1be9acaa8a8476775a666e5a1f879a574fd45b05
zenghongtu <zenghongtu@qq.com>
2018-09-03 22:59:22 +0800

    style: fix eslint style error

--- client/src/components/header/index.vue
+++ client/src/components/header/index.vue
@@ -29,7 +29,7 @@
                 ]
             }
         }
-    }
+    };
 </script>
 
 <style scoped lang="scss">
--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import ajax from "./http/config";
+import ajax from './http/config';
 
 Vue.config.productionTip = false;
 Vue.prototype.$ajax = ajax;
