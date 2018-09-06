54eabb4b39461afc6ae3399cb8821f0ddaf3ee74
zenghongtu <zenghongtu@gmail.com>
2018-09-06 12:52:53 +0800

    feat: add books author mock data

--- client/src/faker/mockDataGenerate.js
+++ client/src/faker/mockDataGenerate.js
@@ -3,15 +3,16 @@
  * Desc: articles
  */
 
-const faker = require('faker/locale/zh_CN');
+const faker = require('faker');
 const fs = require('fs');
 const generateMockData = (count = 30) => {
-    let i = faker.random.number({max: count});
+    let i = faker.random.number({min: 20, max: count});
 
     const articles = [];
     const categories = [];
     const tags = [];
     const titles = [];
+    const books = [];
 
     while (i--) {
         const _title = faker.lorem.sentence();
@@ -26,6 +27,17 @@ const generateMockData = (count = 30) => {
         const _category = faker.hacker.adjective();
         const _title_ = faker.lorem.sentence();
 
+        const _booktitle = faker.lorem.sentence();
+        let j = faker.random.number({min: 1, max: 3});
+        const _articles = [];
+        const _authors = [];
+        while (j--) {
+            const _article = faker.lorem.sentence();
+            const _author = faker.name.firstName() + faker.name.lastName();
+            _articles.push({article: _article, id: j});
+            _authors.push(_author)
+        }
+
         categories.push(_tag);
         tags.push(_category);
         titles.push(_title_);
@@ -39,7 +51,9 @@ const generateMockData = (count = 30) => {
             commentCount: _commentCount,
             createdAt: _createdAt,
             updatedAt: _updatedAt
-        })
+        });
+
+        books.push({title: _booktitle, authors: _authors, articles: _articles})
     }
     return {
         articles,
@@ -47,12 +61,13 @@ const generateMockData = (count = 30) => {
             categories,
             tags,
             titles,
-        }
+        },
+        books
     }
 
 };
 
 
-fs.writeFile(__dirname + '/mockData.json', JSON.stringify(generateMockData(30)), function () {
+fs.writeFile(__dirname + '/mockData.json', JSON.stringify(generateMockData(40)), function () {
     console.log("mockData generated successfully!");
 });
