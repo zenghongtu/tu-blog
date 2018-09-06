439fc445cb4117869fb822dd2c2927642263270f
zenghongtu <zenghongtu@gmail.com>
2018-09-06 14:34:58 +0800

    feat: add lab mock data

--- client/src/faker/mockDataGenerate.js
+++ client/src/faker/mockDataGenerate.js
@@ -13,6 +13,7 @@ const generateMockData = (count = 30) => {
     const tags = [];
     const titles = [];
     const books = [];
+    const projects = {create: [], contribute: []};
 
     while (i--) {
         const _title = faker.lorem.sentence();
@@ -22,25 +23,6 @@ const generateMockData = (count = 30) => {
         const _commentCount = faker.random.number({min: 10, max: 100});
         const _createdAt = faker.date.past();
         const _updatedAt = faker.date.recent();
-
-        const _tag = faker.hacker.noun();
-        const _category = faker.hacker.adjective();
-        const _title_ = faker.lorem.sentence();
-
-        const _booktitle = faker.lorem.sentence();
-        let j = faker.random.number({min: 1, max: 3});
-        const _articles = [];
-        const _authors = [];
-        while (j--) {
-            const _article = faker.lorem.sentence();
-            const _author = faker.name.firstName() + faker.name.lastName();
-            _articles.push({article: _article, id: j});
-            _authors.push(_author)
-        }
-
-        categories.push(_tag);
-        tags.push(_category);
-        titles.push(_title_);
 
         articles.push({
             id: i,
@@ -53,7 +35,56 @@ const generateMockData = (count = 30) => {
             updatedAt: _updatedAt
         });
 
-        books.push({title: _booktitle, authors: _authors, articles: _articles})
+        const _tag = faker.hacker.noun();
+        const _category = faker.hacker.adjective();
+        const _title_ = faker.lorem.sentence();
+
+        categories.push(_tag);
+        tags.push(_category);
+        titles.push(_title_);
+
+        const _booktitle = faker.lorem.sentence();
+        let j = faker.random.number({min: 1, max: 3});
+        const _articles = [];
+        const _authors = [];
+        while (j--) {
+            const _article = faker.lorem.sentence();
+            const _author = faker.name.firstName() + faker.name.lastName();
+            _articles.push({article: _article, id: j});
+            _authors.push(_author)
+        }
+
+        books.push({title: _booktitle, authors: _authors, articles: _articles});
+
+        const _projectName = faker.name.title();
+        const _projectUrl = faker.internet.url();
+        const _starNum = faker.random.number({min: 100, max: 1000});
+        const _forkNum = faker.random.number({min: 100, max: 1000});
+        const _projectDesc = faker.lorem.sentence();
+        const _projectArticles = [];
+        let k = faker.random.number({min: 1, max: 3});
+        while (k--) {
+            const _article = faker.lorem.sentence();
+            _projectArticles.push({article: _article, id: k});
+        }
+        faker.random.boolean() ?
+            projects.create.push({
+                name: _projectName,
+                url: _projectUrl,
+                star: _starNum,
+                fork: _forkNum,
+                desc: _projectDesc,
+                articles: _projectArticles
+            })
+            :
+            projects.contribute.push({
+                name: _projectName,
+                url: _projectUrl,
+                star: _starNum,
+                fork: _forkNum,
+                desc: _projectDesc,
+                articles: _projectArticles
+            })
     }
     return {
         articles,
@@ -62,9 +93,9 @@ const generateMockData = (count = 30) => {
             tags,
             titles,
         },
-        books
+        books,
+        projects
     }
-
 };
 
 
