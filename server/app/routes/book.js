b94f1b0d7b2210b83a8d6b34101c1d21cc8d863d
zenghongtu <zenghongtu@qq.com>
2018-09-13 23:09:22 +0800

    feat: add book controller route

---
+++ server/app/routes/book.js
@@ -0,0 +1,27 @@
+/**
+ * Created by zenghongtu on 13/09/2018
+ * Desc: book
+ */
+
+import Router from 'koa-router';
+import {baseApi} from '../config';
+import jwt from '../middlewares/jwt';
+import BookControllers from '../controllers/book';
+
+const api = 'books';
+
+const router = new Router();
+
+router.prefix(`${baseApi}/${api}`);
+
+router.get('/', BookControllers.find);
+
+router.post('/', jwt, BookControllers.add);
+
+router.get('/:id', BookControllers.findById);
+
+router.put('/:id', jwt, BookControllers.update);
+
+router.delete('/:id', jwt, BookControllers.delete);
+
+export default router;
