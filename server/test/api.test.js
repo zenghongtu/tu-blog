741038e25ff928f1f0d0fabff1b5fd76b03d5501
zenghongtu <zenghongtu@gmail.com>
2018-09-20 17:07:20 +0800

    feat: add api.test.js

--- server/test/api.test.js
+++ server/test/api.test.js
@@ -1,163 +1,181 @@
-import app from '../app/';
+import app from '../app/index';
 import supertest from 'supertest';
 import {expect, should} from 'chai';
 
-const temp = {};
 const request = supertest.agent(app.listen());
-should();
 
-const name = 'tu';
-const password = '123';
+const temp = {
+    name: 'tu',
+    password: '123',
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjViYTM0ZjEzODZmNDkxMjIzNmVkYjUwYiIsIm5hbWUiOiJ0dSIsInJvbGUiOjAsImlhdCI6MTUzNzQzMjg5MywiZXhwIjoxNTM3NDc2MDkzfQ.acy1Outhpwd8dm4EQCMKV7slN4DbG0XxzCxoUuwJ0QE',
+    _id: "5ba35efad039e72523adf788",
+    categories: [],
+    tags: [],
+    articles: [],
+};
 
-describe('POST api/users', () => {
-    it('should get user info', done => {
+
+// describe('POST /users', () => {
+//     it('should name equal and password not equal', done => {
+//         request
+//             .post('/users')
+//             .set('Accept', 'application/json')
+//             .send({
+//                 "name": temp.name,
+//                 "email": "zenghongtu@gmail.com",
+//                 "password": temp.password
+//             })
+//             .expect(200, (err, res) => {
+//                 const _body = res.body
+//                 expect(_body.name).to.be.equal(temp.name)
+//                 expect(_body.password).to.not.be.equal(temp.password)
+//                 done();
+//             });
+//     });
+// });
+
+// describe('POST /authenticate', () => {
+//     it('should get token', done => {
+//         request
+//             .post('/authenticate')
+//             .set('Accept', 'application/json')
+//             .send({
+//                 name: temp.name,
+//                 password: temp.password,
+//             })
+//             .expect(200, (err, res) => {
+//                 expect(res.body.token).to.be.a('string').not.to.be.empty;
+//                 temp.token = res.body.token;
+//                 done();
+//             });
+//     });
+// });
+
+// describe('POST /categories', () => {
+//     it('should add categories', done => {
+//         let i = 5;
+//         while (i--) {
+//             request
+//                 .post('/categories')
+//                 .set('Accept', 'application/json')
+//                 .set('Authorization', `Bearer ${temp.token}`)
+//                 .set('Accept', 'application/json')
+//                 .send({
+//                     "name": 'cat' + i
+//                 })
+//                 .expect(200, (err, res) => {
+//                     expect(res.body).to.be.an('object').not.to.empty;
+//                 });
+//         }
+//         done()
+//     });
+// });
+
+
+// describe('POST /tags', () => {
+//     it('should add tags', done => {
+//         let i = 5;
+//         while (i--) {
+//             request
+//                 .post('/tags')
+//                 .set('Accept', 'application/json')
+//                 .set('Authorization', `Bearer ${temp.token}`)
+//                 .set('Accept', 'application/json')
+//                 .send({
+//                     "name": 'tag' + i
+//                 })
+//                 .expect(200, (err, res) => {
+//                     expect(res.body).to.be.an('object').not.to.empty;
+//                 });
+//         }
+//         done()
+//     });
+// });
+
+
+describe('GET /categories', () => {
+    it('should get categories', done => {
         request
-            .post('/api/users')
-            .set('Accept', 'application/json')
-            .send({
-                "name": 'name',
-                "email": "zenghongtu@gmail.com",
-                "password": password
-            })
+            .get('/categories')
+            .set('_id', temp._id)
             .expect(200, (err, res) => {
-                res.body.name.should.eql('name', '不相等');
+                // const _id = res.headers._id
+                // expect(_id).not.to.be.empty;
+                // temp._id = _id
+                const _body = res.body;
+                expect(_body).not.to.be.empty;
+                temp.categories = _body;
+                done()
+            });
+    })
+});
+
+describe('GET /tags', () => {
+    it('should get tags', done => {
+        request
+            .get('/tags')
+            .set('_id', temp._id)
+            .expect(200, (err, res) => {
+                const _body = res.body;
+                expect(_body).not.to.be.empty;
+                temp.tags = _body;
+                done()
+            });
+    })
+});
+
+// describe('POST /articles', () => {
+//     it('should add article', done => {
+//         let i = 5;
+//         while (i--) {
+//             request
+//                 .post('/articles')
+//                 .set('Accept', 'application/json')
+//                 .set('Authorization', `Bearer ${temp.token}`)
+//                 .set('Accept', 'application/json')
+//                 .send({
+//                     "title": "a test " + i,
+//                     "desc": "this is a test",
+//                     "body": "this is a body",
+//                     "category": temp.categories[i],
+//                     "tags": [
+//                         temp.tags[i]
+//                     ],
+//                     "isPublish": true
+//                 })
+//                 .expect(200, (err, res) => {
+//                     expect(res.body).to.be.an('object').not.to.empty;
+//                 });
+//         }
+//         done()
+//     });
+// });
+
+describe('GET /articles', () => {
+    it('should get articles', done => {
+        request
+            .get('/articles')
+            .set('_id', temp._id)
+            .expect(200, (err, res) => {
+                const _body = res.body;
+                expect(_body).not.to.be.empty;
+                temp.articles = _body;
+                done()
+            });
+    })
+});
+
+describe('GET /site', () => {
+    it('pv>=1 and uv>=1 and visits>=1', done => {
+        request
+            .get('/site')
+            .set('_id', temp._id)
+            .expect(200, (err, res) => {
+                expect(res.body.site.pv).to.be.at.least(1);
+                expect(res.body.site.uv).to.be.at.least(1);
+                expect(res.body.visitor.visits).to.be.at.least(1);
                 done();
             });
     });
 });
 
-describe('POST api/authenticate', () => {
-    it('should get all cities', done => {
-        request
-            .post('/api/authenticate')
-            .set('Accept', 'application/json')
-            .send({
-                password: 'password'
-            })
-            .expect(200, (err, res) => {
-                temp.token = res.body.token;
-                done();
-            });
-    });
-});
-
-
-describe('POST api/authenticate', () => {
-    it('should get all cities', done => {
-        request
-            .post('/api/authenticate')
-            .set('Accept', 'application/json')
-            .send({
-                password: 'password'
-            })
-            .expect(200, (err, res) => {
-                temp.token = res.body.token;
-                done();
-            });
-    });
-});
-
-describe('POST /city', () => {
-    it('should add a city', done => {
-        request
-            .post('/api/cities')
-            .set('Accept', 'application/json')
-            .set('Authorization', `Bearer ${temp.token}`)
-            .set('Accept', 'application/json')
-            .send({
-                name: 'Bangkok',
-                totalPopulation: 8249117,
-                country: 'Thailand',
-                zipCode: 1200
-            })
-            .expect(200, (err, res) => {
-                temp.idCity = res.body._id;
-                done();
-            });
-    });
-});
-
-describe('GET /cities', () => {
-    it('should get all cities', done => {
-        request
-            .get('/api/cities')
-            .set('Authorization', `Bearer ${temp.token}`)
-            .set('Accept', 'application/json')
-            .expect(200, (err, res) => {
-                expect(res.body.length).to.be.at.least(1);
-                done();
-            });
-    });
-});
-
-describe('GET /cities/:id', () => {
-    it('should get a city', done => {
-        request
-            .get(`/api/cities/${temp.idCity}`)
-            .set('Authorization', `Bearer ${temp.token}`)
-            .set('Accept', 'application/json')
-            .expect(200, (err, res) => {
-                res.body.name.should.equal('Bangkok');
-                res.body.totalPopulation.should.equal(8249117);
-                res.body.country.should.equal('Thailand');
-                res.body.zipCode.should.equal(1200);
-                res.body._id.should.equal(temp.idCity);
-                done();
-            });
-    });
-});
-
-describe('PUT /cities', () => {
-    it('should update a city', done => {
-        request
-            .put(`/api/cities/${temp.idCity}`)
-            .set('Authorization', `Bearer ${temp.token}`)
-            .set('Accept', 'application/json')
-            .send({
-                name: 'Chiang Mai',
-                totalPopulation: 148477,
-                country: 'Thailand',
-                zipCode: 50000
-            })
-            .expect(200, (err, res) => {
-                temp.idCity = res.body._id;
-                done();
-            });
-    });
-
-    it('should get updated city', done => {
-        request
-            .get(`/api/cities/${temp.idCity}`)
-            .set('Authorization', `Bearer ${temp.token}`)
-            .set('Accept', 'application/json')
-            .expect(200, (err, res) => {
-                res.body.name.should.equal('Chiang Mai');
-                res.body.totalPopulation.should.equal(148477);
-                res.body.country.should.equal('Thailand');
-                res.body.zipCode.should.equal(50000);
-                res.body._id.should.equal(temp.idCity);
-                done();
-            });
-    });
-});
-
-describe('DELETE /cities', () => {
-    it('should delete a city', done => {
-        request
-            .delete(`/api/cities/${temp.idCity}`)
-            .set('Authorization', `Bearer ${temp.token}`)
-            .set('Accept', 'application/json')
-            .expect(200, (err, res) => {
-                done();
-            });
-    });
-
-    it('should get error', done => {
-        request
-            .get(`/api/cities/${temp.idCity}`)
-            .set('Accept', 'application/json')
-            .expect(404, () => {
-                done();
-            });
-    });
-});
