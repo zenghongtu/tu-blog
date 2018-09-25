94a34c0df269f904fd803c9dfe8e49091f86eace
zenghongtu <zenghongtu@qq.com>
2018-09-26 00:21:07 +0800

    feat: add blink icon

--- client/src/components/footer/index.vue
+++ client/src/components/footer/index.vue
@@ -5,9 +5,8 @@
 
 <template>
     <div class="footer">
-        <div class="notice" v-show="ida" @click="clickHandler">
-            <!--todo-->
-            <i>消息</i>
+        <div :class="'notice '+ (ida ? 'blink':'')" v-show="true" @click="clickHandler" :title="msg">
+            <i class="iconfont icon-tixing1 notice-icon"></i>
         </div>
         <div class="footer-wrap">
             Copyright © 2018 <a class="link" href="https://zenghongtu.com">这个码农不太冷</a> | <a class="link" target="_blank"
@@ -22,16 +21,19 @@
         data() {
             return {
                 ida: null,
+                msg: '暂无留言消息,快去文章下面写一条吧~',
             }
         },
         methods: {
             clickHandler() {
-                this.$router.push({name: 'article', params: {_id: this._ida}})
+                if (this._ida) {
+                    this.$router.push({name: 'article', params: {_id: this._ida}})
+                }
             }
         },
         created() {
             const _ida = window.localStorage.getItem('_ida');
-            _ida && (this.ida = _ida)
+            _ida && (this.ida = _ida) && (this.msg = '有一条留言被回复了哦,点击前往查看')
         }
     }
 </script>
@@ -57,6 +59,25 @@
             bottom: 3em;
             right: 3em;
             cursor: pointer;
+            color: $base;
+            .notice-icon {
+                font-size: 2em;
+            }
+            &.blink {
+                animation: blink 1s infinite;
+            }
+        }
+    }
+
+    @keyframes blink {
+        0% {
+            color: $btn-active-border;
+        }
+        50% {
+            color: $active;
+        }
+        100% {
+            color: $btn-active-border;
         }
     }
 </style>
