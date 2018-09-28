e747217f535117e5db68c00a4ddadf27e405dfb6
zenghongtu <zenghongtu@gmail.com>
2018-09-28 09:57:39 +0800

    fix(client): view

--- client/src/router/views.js
+++ client/src/router/views.js
@@ -5,15 +5,15 @@
 
 const views = {
     Read: () => import(/* webpackChunkName: "login" */'@/views/read/index.vue'),
-    Explore: () => import(/* webpackChunkName: "read " */'@/views/explore/index.vue'),
-    Archive: () => import(/* webpackChunkName: "explore " */'@/views/archive/index.vue'),
-    About: () => import(/* webpackChunkName: "archive " */'@/views/about/index.vue'),
-    Laboratory: () => import(/* webpackChunkName: "about " */'@/views/laboratory/index.vue'),
-    Home: () => import(/* webpackChunkName: "laboratory " */'@/views/home/index.vue'),
-    Article: () => import(/* webpackChunkName: "home " */'@/views/article/index.vue'),
-    Category: () => import(/* webpackChunkName: "article " */'@/views/category/index.vue'),
-    Tag: () => import(/* webpackChunkName: "category " */'@/views/tag/index.vue'),
-    NotFound: () => import(/* webpackChunkName: "category " */'@/views/not-found/index.vue'),
+    Explore: () => import(/* webpackChunkName: "read" */'@/views/explore/index.vue'),
+    Archive: () => import(/* webpackChunkName: "explore" */'@/views/archive/index.vue'),
+    About: () => import(/* webpackChunkName: "archive" */'@/views/about/index.vue'),
+    Laboratory: () => import(/* webpackChunkName: "about" */'@/views/laboratory/index.vue'),
+    Home: () => import(/* webpackChunkName: "laboratory" */'@/views/home/index.vue'),
+    Article: () => import(/* webpackChunkName: "home" */'@/views/article/index.vue'),
+    Category: () => import(/* webpackChunkName: "article" */'@/views/category/index.vue'),
+    Tag: () => import(/* webpackChunkName: "category" */'@/views/tag/index.vue'),
+    NotFound: () => import(/* webpackChunkName: "category" */'@/views/not-found/index.vue'),
 };
 
 export default views
